fb09b5399476f4bff87c30622b18c9758c9cdcc5
Kim Gonzales <kimgo589@gmail.com>
2018-01-23 21:14:57 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -6,3 +6,14 @@ def countdown(number)
   end
   "HAPPY NEW YEAR!"
 end
+
+
+def countdown_with_sleep(number)
+  number = 10
+  while number > 0
+    sleep(1)
+    puts "#{number} SECOND(S)!"
+    number -=1
+  end
+  "HAPPY NEW YEAR!"
+end
